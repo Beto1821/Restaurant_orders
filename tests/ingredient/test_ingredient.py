c024fa3c3c6d3aa295d4e5189402cac2128e3dfb
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-05-16 11:44:37 -0300

    fixed: req01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -2,35 +2,22 @@ from src.models.ingredient import Ingredient, Restriction
 
 
 def test_ingredient():
-    # Req 1
+
     ingredient = Ingredient("queijo mussarela")
-    assert ingredient.name == "queijo mussarela"
-    assert ingredient.restrictions == {
-        Restriction.LACTOSE,
-        Restriction.ANIMAL_DERIVED,
-    }
-
-    # Req 2
-    assert repr(ingredient) == "Ingredient('queijo mussarela')"
-
-    # Req 3
-    ingredient1 = Ingredient("queijo mussarela")
-    assert ingredient1 == ingredient1
-
-    # Req 4
     ingredient2 = Ingredient("queijo gorgonzola")
-    assert ingredient1 != ingredient2
 
-    # Req 5
-    assert hash(ingredient1) == hash(ingredient1)
-
-    # Req 6
-    assert hash(ingredient1) != hash(ingredient2)
-
-    # Req 7
     assert ingredient.name == "queijo mussarela"
 
-    # Req 8
+    assert ingredient.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert ingredient == ingredient
+
+    assert ingredient != ingredient2
+
+    assert hash(ingredient) == hash("queijo mussarela")
+
+    assert hash(ingredient) != hash(ingredient2)
+
     assert ingredient.restrictions == {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,
