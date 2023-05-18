0ede8f373ffcd1420e0877d11a0bb5590600cb14
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-05-17 23:15:40 -0300

    Feat: req02

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,37 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    # Teste de instanciação e atributos do prato
+    lasanha = Dish('Lasanha', 15.99)
+    assert lasanha.name == 'Lasanha'
+    assert lasanha.__repr__() == "Dish('Lasanha', R$15.99)"
+
+    # Teste de igualdade e hash de pratos iguais
+    lasanha2 = Dish('Lasanha', 15.99)
+    assert lasanha == lasanha2
+    assert lasanha.__hash__() == lasanha2.__hash__()
+
+    # Teste de desigualdade e hash de pratos diferentes
+    sushi = Dish('Sushi', 12.99)
+    assert lasanha != sushi
+    assert lasanha.__hash__() != sushi.__hash__()
+
+    # Teste de exceções ao criar prato com valores inválidos
+    with pytest.raises(TypeError):
+        Dish('name', 'price')
+    with pytest.raises(ValueError):
+        Dish('name', 0)
+
+    # Teste de adição de ingredientes à receita do prato
+    lasanha.add_ingredient_dependency(Ingredient('Massa de Lasanha'), 8)
+    assert lasanha.recipe.get(Ingredient('Massa de Lasanha')) == 8
+    assert lasanha.get_restrictions() == set()
+
+    # Teste de obtenção dos ingredientes da receita e restrições do prato
+    lasanha.add_ingredient_dependency(Ingredient('Queijo'), 2)
+    assert lasanha.get_ingredients() == {Ingredient('Massa de Lasanha'),
+                                         Ingredient('Queijo')}
+    assert lasanha.get_restrictions() == set()
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -2,22 +2,29 @@ from src.models.ingredient import Ingredient, Restriction
 
 
 def test_ingredient():
-
+    # Criação de instâncias de Ingredient
     ingredient = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("queijo gorgonzola")
 
+    # Verificações do atributo name
     assert ingredient.name == "queijo mussarela"
 
+    # Verificação da representação em string
     assert ingredient.__repr__() == "Ingredient('queijo mussarela')"
 
+    # Verificação de igualdade do próprio objeto
     assert ingredient == ingredient
 
+    # Verificação de desigualdade entre objetos diferentes
     assert ingredient != ingredient2
 
+    # Verificação do valor de hash do objeto ingredient
     assert hash(ingredient) == hash("queijo mussarela")
 
+    # Verificação de valores de hash diferentes para objetos diferentes
     assert hash(ingredient) != hash(ingredient2)
 
+    # Verificação das restrições do objeto ingredient
     assert ingredient.restrictions == {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,
