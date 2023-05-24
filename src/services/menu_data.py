422bf8b1be2d4ee4d8d9a8e994b22ec07757a135
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-05-23 21:25:08 -0300

    feat req03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,32 @@
-# Req 3
+import csv
+from models.ingredient import Ingredient
+from models.dish import Dish
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            header, *data = csv_reader
+            dishes = {}  # Dictionary to store dishes
+
+            for item in data:
+                dish_name = item[0]
+                price = float(item[1])
+                ingredient_name = item[2]
+                quantity = int(item[3])
+
+                if dish_name not in dishes:
+                    # Create a new Dish object if it doesn't exist
+                    dishes[dish_name] = Dish(dish_name, price)
+
+                # Get the Dish object from the dictionary
+                dish = dishes[dish_name]
+                # Create an Ingredient object
+                ingredient = Ingredient(ingredient_name)
+                # Add ingredient and quantity as a dependency to the dish
+                dish.add_ingredient_dependency(ingredient, quantity)
+
+            # Convert the dictionary values to a set
+            # and assign it to self.dishes
+            self.dishes = set(dishes.values())
